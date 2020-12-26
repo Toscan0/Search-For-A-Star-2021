66a2f6ec781e137e9fd457dfe35508e9618865f3
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-12-26 19:27:15 +0000

    background logic implemented

---
+++ UFO-Game/Assets/Scripts/BackgroundMusic/BackgroundMusic.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BackgroundMusic : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] clips;
+    private int currentClip = 0;
+    [SerializeField]
+    private bool stopPlaying = false;
+    public bool StopPlaying { 
+        get
+        {
+            return stopPlaying;
+        }
+        private set
+        {
+            stopPlaying = value;
+
+            audioSource.enabled = !stopPlaying;
+        }
+    }
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        currentClip = UnityEngine.Random.Range(0, clips.Length);
+        return clips[currentClip];
+    }
+
+    private AudioClip GetClipByIndex(int i)
+    {
+        currentClip = i;
+        return clips[i];
+    }
+
+    private AudioClip GetNextClip()
+    {
+        currentClip++;
+
+        if(currentClip >= clips.Length)
+        {
+            currentClip = 0;
+        }
+
+        return clips[currentClip];
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying && clips.Length > 0 && StopPlaying == false)
+        {
+            audioSource.clip = GetNextClip();
+            audioSource.Play();
+        }
+    }
+}
