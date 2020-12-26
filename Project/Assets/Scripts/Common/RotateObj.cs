8805ae9b5edcee3a0c4e6ab8860e86ca7e86f9cd
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2020-12-26 15:32:42 +0000

    project cleaned, gold prefab done

---
+++ Project/Assets/Scripts/Common/RotateObj.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObj : MonoBehaviour
+{
+    [SerializeField]
+    private float angle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * angle, 0, Space.World);
+    }
+}
--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Project made for Search For a start of Grade in Games in 2021
 ## Requirements
 
 * Unity 2020.1.16f1
+
+
+* TODO:
+* Gold: space ship
+* Hart
+* Musics
+* Intro anim
